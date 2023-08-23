0fb115111da066b208ab60a30e5ac248024073e3
bkomarath <bkomarath@rbgo.in>
2023-08-23 11:32:31 +0530

    Generalizing permutations. Derangements.

--- combinatorics.c
+++ combinatorics.c
@@ -36,8 +36,39 @@ void permutations(int a[], int n, int start)
     }
 }
 
+void process_permutations(int a[], int n, int start, void (*process_perm)(int a[], int n))
+{
+    if (start == n-1) {
+        (*process_perm)(a, n);
+        return;
+    }
+
+    // Generate all permutations with a[start] at the beginning.
+    process_permutations(a, n, start+1, process_perm);
+
+    for (int i = start+1; i < n; ++i) {
+        // Now, bring the other elements to the front.
+        swap(&a[start], &a[i]);
+        process_permutations(a, n, start+1, process_perm);
+        swap(&a[start], &a[i]);
+    }
+}
+
+int is_derangement(int a[], int n)
+{
+    for (int i = 0; i < n; ++i) {
+        if (a[i] == i) { return 0; }
+    }
+    return 1;
+}
+
+void print_if_derangement(int a[], int n)
+{
+    if (is_derangement(a, n)) { print_array(a, n); }
+}
+
 int main()
 {
-    permutations((int[]){0, 1, 2, 3}, 4, 0);
+    process_permutations((int[]){0, 1, 2, 3}, 4, 0, print_if_derangement);
     return 0;
 }
