ee3d420f05fbdd1c4a2a6823d99e4e4d2cda647d
bkomarath <bkomarath@rbgo.in>
2023-10-09 12:13:42 +0530

    eight-puzzle using BFS.

--- puzzle8.cpp
+++ puzzle8.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 #include <vector>
+#include <array>
+#include <algorithm>
 
 #include "queue.hpp"
 
 struct board {
     int e[3][3];
-    int r, c;
 };
 
 constexpr int fact(int i)
@@ -39,6 +40,25 @@ int ord(const board& board)
     return a;
 }
 
+void print_board(const board& b)
+{
+    for (int r = 0; r < 3; ++r) {
+        for (int c = 0; c < 3; ++c) {
+            printf("%3d", b.e[r][c]);
+        }
+        printf("\n");
+    }
+}
+
+void read_board(board& b)
+{
+    for (int r = 0; r < 3; ++r) {
+        for (int c = 0; c < 3; ++c) {
+            scanf("%d", &b.e[r][c]);
+        }
+    }
+}
+
 void swap(int &a, int &b)
 {
     int t = a;
@@ -46,46 +66,145 @@ void swap(int &a, int &b)
     b = t;
 }
 
+std::tuple<int, int> find_space(const board& b)
+{
+    for (int r = 0; r < 3; ++r)
+        for (int c = 0; c < 3; ++c)
+            if (b.e[r][c] == 9) return { r, c };
+    assert(0);
+}
+
 board up(const board& b)
 {
-    if (b.r == 0) return b;
-    board c = b;
-    swap(c.e[b.r-1][b.c], c.e[b.r][b.c]);
-    return c;
+    const auto [r, c] = find_space(b);
+    if (r == 0) return b;
+    board o = b;
+    swap(o.e[r-1][c], o.e[r][c]);
+    return o;
 }
 
 board down(const board& b)
 {
-    if (b.r == 2) return b;
-    board c = b;
-    swap(c.e[b.r+1][b.c], c.e[b.r][b.c]);
-    return c;
+    const auto [r, c] = find_space(b);
+    if (r == 2) return b;
+    board o = b;
+    swap(o.e[r+1][c], o.e[r][c]);
+    return o;
 }
 
 board left(const board& b)
 {
-    if (b.c == 0) return b;
-    board c = b;
-    swap(c.e[b.r][b.c-1], c.e[b.r][b.c]);
-    return c;
+    const auto [r, c] = find_space(b);
+    if (c == 0) return b;
+    board o = b;
+    swap(o.e[r][c-1], o.e[r][c]);
+    return o;
 }
 
 board right(const board& b)
 {
-    if (b.c == 2) return b;
-    board c = b;
-    swap(c.e[b.r][b.c+1], c.e[b.r][b.c]);
-    return c;
+    const auto [r, c] = find_space(b);
+    if (c == 2) return b;
+    board o = b;
+    swap(o.e[r][c+1], o.e[r][c]);
+    return o;
 }
+
+bool is_same(const board& a, const board &b)
+{
+    for (int r = 0; r < 3; ++r)
+        for (int c = 0; c < 3; ++c)
+            if (a.e[r][c] != b.e[r][c]) return false;
+
+    return true;
+}
+
+enum move { L = 1, R = 2, U = 3, D = 4 };
 
 /*
  * Return a shortest path from src to dest.
  */
-std::vector<board> solve(const board& src, const board& dest)
+std::vector<int> solve(const board& src, const board& dest)
 {
+    queue<board, fact(9)> q;
+    int visited[fact(9)];
+    board parent[fact(9)];
+
+    enqueue(q, src);
+    visited[ord(src)] = L;
+
+    while (!is_empty(q)) {
+        board u = dequeue(q);
+        if (is_same(u, dest)) {
+            /* return the moves to get to u from src. */
+            std::vector<int> moves;
+            board c = u;
+            int o = ord(c);
+            while (!is_same(c, src)) {
+                moves.push_back(visited[o]);
+                c = parent[o];
+                o = ord(c);
+            }
+            std::reverse(moves.begin(), moves.end());
+            return moves;
+        }
+
+        board a = up(u);
+        board b = down(u);
+        board c = left(u);
+        board d = right(u);
+
+        int aord = ord(a);
+        int bord = ord(b);
+        int cord = ord(c);
+        int dord = ord(d);
+
+        if (!visited[aord]) {
+            visited[aord] = U;
+            parent[aord] = u;
+            enqueue(q, a);
+        }
+        if (!visited[bord]) {
+            visited[bord] = D;
+            parent[bord] = u;
+            enqueue(q, b);
+        }
+        if (!visited[cord]) {
+            visited[cord] = L;
+            parent[cord] = u;
+            enqueue(q, c);
+        }
+        if (!visited[dord]) {
+            visited[dord] = R;
+            parent[dord] = u;
+            enqueue(q, d);
+        }
+    }
+    assert(0);
+}
+
+void print_moves(const std::vector<int>& moves)
+{
+    for (auto m: moves) {
+        switch (m) {
+        case L: printf("L "); break;
+        case R: printf("R "); break;
+        case U: printf("U "); break;
+        case D: printf("D "); break;
+        }
+    }
+    printf("\n");
 }
 
 int main()
 {
+    board src, dest;
+
+    read_board(src);
+    read_board(dest);
+
+    auto moves = solve(src, dest);
+    print_moves(moves);
+
     return 0;
 }
